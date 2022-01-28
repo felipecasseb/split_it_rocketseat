a66ac196d60d37166f1a7fb36e5531f610dbeeb5
felipecasseb <felipe.casseb@hotmail.com>
2022-01-28 15:57:43 -0300

    Initial commit

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:split_it/theme/app_colors.dart';
+import 'package:split_it/theme/app_gradients.dart';
+
+class AppTheme{
+  static AppColors get colors => AppColorDefault();
+  static AppGradients get gradients => AppGradientDefault();
+}
